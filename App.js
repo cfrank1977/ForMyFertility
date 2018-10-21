cdfabad6fdbd33dfbae0a50a1d7a43db1d88f941
ipointer2 <chris.e.frank@gmail.com>
2018-10-21 13:18:53 -0400

    Auth, login, navigation, and saving answers to Redux store work.

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import { Provider, connect } from 'react-redux';
 
+import Amplify, { Auth } from 'aws-amplify' 
+import config from './aws-exports' 
+import AppSyncConfig from './appsync-config'
+Amplify.configure({ ...config, ...AppSyncConfig })
+
+import { Provider } from 'react-redux';
 import configureStore from './src/store/configureStore';
 import initialState from './src/reducers/initialState';
-import FertilityForm from './src/components/fertility/fertilityForm';
+
+import HomeScreen from './src/components/home/index'
 
 const store = configureStore(initialState);
 
-// const ConnectedUserForm = connect(null)(fertilityForm);
-
 export default class App extends Component {
+  state = {
+    isAuthenticated: false
+  }
+
+  authenticate(isAuthenticated) {
+    this.setState({ isAuthenticated })
+    this.props.navigation.navigate('App');
+  }
+
   render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated
+    };
+
     return (
       <Provider store={store}>
-        <FertilityForm />
+        <HomeScreen screenProps={{
+              authenticate: this.authenticate.bind(this)
+              }}/>
       </Provider>
     );
   }
