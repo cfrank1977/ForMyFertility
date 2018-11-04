b71c23712beaf1007cb11baa3a245fe9acdb3280
ipointer2 <chris.e.frank@gmail.com>
2018-11-04 11:59:47 -0500

    able to access redux state.

--- src/components/fertility/Report.js
+++ src/components/fertility/Report.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import {
     Content,
     Text,
     View
 } from 'native-base';
+import { connect } from 'react-redux';
+import { Form, Control } from 'react-redux-form/native';
 
-
-
-export default class Report extends Component {
-    render() {
-        return (
-            <Content >
-                <View >
-                    <Text>The Report</Text>
-                </View>
-            </Content>
-        );
-    }
+let Report = (props) => {
+    return (
+        <Content >
+            <View >
+                <Text>The Report</Text>
+            </View>
+            <Form model="fertilityQuestions">
+            <Text>You were born in.</Text>
+                <Control.TextInput
+                    model=".age"
+                />
+                <Text>You've been trying for at least a year.</Text>
+                <Control.TextInput
+                    model=".yearChildlessSex"
+                />
+                <Text>You've been pregnant before.</Text>
+                <Control.TextInput
+                    model=".hadPregnancy"
+                />
+            </Form>
+        </Content>
+    );
 }
+
+export default connect(({ fertilityQuestions }) => ({
+    age: fertilityQuestions.age,
+    yearChildlessSex: fertilityQuestions.yearChildlessSex,
+    hadPregnancy: fertilityQuestions.hadPregnancy
+}))(Report);
--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,5 +1,5 @@
 export default {
-    Age: '',
+    age: '',
     yearChildlessSex: '',
     hadPregnancy: '',
     miscarriages: '',
