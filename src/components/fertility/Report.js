5a1595eadd7c1c7cc814e5b1e27bdf6573f358d5
ipointer2 <chris.e.frank@gmail.com>
2018-11-16 10:33:48 -0500

    on report compenentDidMount get username post questions to database

--- src/components/fertility/Report.js
+++ src/components/fertility/Report.js
@@ -15,23 +15,16 @@ class Report extends Component {
         super(props, context);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
-          };
+        };
     }
 
     async componentDidMount() {
-        
-        Auth.currentAuthenticatedUser()
-        .then((user) => {
-            this.setState(user);
-            console.log(this.state.username);
-          }).catch((error)=>{
-             console.log("No Authenticated User");
-             console.log(error.message);
-          });
-    }
+        // const data = await API.get('dev-formyfertilityapi', '/formyfertility/186711b9-e4c4-4e4b-b5f3-aa0d33704fac')
+        // console.log('Chris id ending fac: ', data)
 
-    handleSubmit() {
-        
+        const username = await Auth.currentAuthenticatedUser();
+        this.setState(username);
+        console.log(`chris the username is: ${JSON.stringify(this.state.username)}`)
         let query = `
             mutation add {
                 createQuestions(input: {
@@ -69,9 +62,73 @@ class Report extends Component {
                 }) { id }
             }
         `
-        console.log(query)
-        API.graphql(graphqlOperation(query))
+        const results = await API.graphql(graphqlOperation(query));
+        this.setState(results.data);
+        console.log(`chris the DynamoDB id is: ${JSON.stringify(this.state.createQuestions.id)}`)
+        // API.graphql(graphqlOperation(query))
+        //     .then((queryResult) => {
+        //         this.setState(queryResult.data);
+        //         console.log(this.state.createQuestions.id);
+        //     }).catch((error) => {
+        //         console.log("No Authenticated User");
+        //         console.log(error.message);
+        //     });
+        // Auth.currentAuthenticatedUser()
+        //     .then((user) => {
+        //         this.setState(user);
+        //         console.log(this.state.username);
+        //     }).catch((error) => {
+        //         console.log("No Authenticated User");
+        //         console.log(error.message);
+        //     });
+    }
 
+    handleSubmit() {
+        let query = `
+            mutation add {
+                createQuestions(input: {
+                    username: "${this.state.username}",
+                    gender: "female",
+                    age: ${this.props.fertilityQuestions.age},
+                    yearChildlessSex: "${this.props.fertilityQuestions.yearChildlessSex}",
+                    amountYearsChildlessSex: ${this.props.fertilityQuestions.amountYearsChildlessSex},
+                    currentIVF: "${this.props.fertilityQuestions.currentIVF}",
+	                hadPregnancy: "${this.props.fertilityQuestions.hadPregnancy}",
+	                hadEctopicPregnancy: "${this.props.fertilityQuestions.hadEctopicPregnancy}",
+	                liveBirth: "${this.props.fertilityQuestions.liveBirth}",
+	                miscarriages: "${this.props.fertilityQuestions.miscarriages}",
+	                partner: "${this.props.fertilityQuestions.partner}",
+                    whichPartnerIssue: "${this.props.fertilityQuestions.whichPartnerIssue}"
+                    amountChildren: ${this.props.fertilityQuestions.amountChildren},
+    	            amountperfecthealth: ${this.props.fertilityQuestions.amountperfecthealth},
+    	            donorsperm: "${this.props.fertilityQuestions.donorsperm}",
+   		            donoreggs: "${this.props.fertilityQuestions.donoreggs}",
+    	            donorembryos: "${this.props.fertilityQuestions.donorembryos}",
+    	            eggs: ${this.props.fertilityQuestions.eggs},
+    	            embryos: ${this.props.fertilityQuestions.embryos},
+    	            embryostransfered: ${this.props.fertilityQuestions.embryostransfered},
+    	            embryosfinalcycle: ${this.props.fertilityQuestions.embryosfinalcycle},
+    	            frozenembryos: "${this.props.fertilityQuestions.frozenembryos}",
+    	            gynecologicalCauses: "${this.props.fertilityQuestions.gynecologicalCauses}",
+    	            gestationalcarrier: "${this.props.fertilityQuestions.gestationalcarrier}",
+    	            hormone: "${this.props.fertilityQuestions.hormone}",
+    	            icsi: "${this.props.fertilityQuestions.icsi}",
+    	            ivfcycles: ${this.props.fertilityQuestions.ivfcycles},
+    	            ivfconceived: "${this.props.fertilityQuestions.ivfconceived}",
+    	            morethanoneivfconceived: "${this.props.fertilityQuestions.morethanoneivfconceived}",
+    	            singletonmulitbirth: "${this.props.fertilityQuestions.singletonmulitbirth}",
+    	            whichGynecologicalCauses: "${this.props.fertilityQuestions.whichGynecologicalCauses}"
+                }) { id }
+            }
+        `
+        API.graphql(graphqlOperation(query))
+            .then((queryResult) => {
+                this.setState(queryResult.data);
+                console.log(this.state.createQuestions.id);
+            }).catch((error) => {
+                console.log("No Authenticated User");
+                console.log(error.message);
+            });
     }
 
     render() {
@@ -86,7 +143,7 @@ class Report extends Component {
 
                     <View>
                         <Button type="submit" onPress={this.handleSubmit}>
-                            <Text> Get Report , {this.props.fertilityQuestions.age} {this.props.fertilityQuestions.yearChildlessSex}!</Text>
+                            <Text> Get Report!</Text>
                         </Button>
                     </View>
                 </Form>
