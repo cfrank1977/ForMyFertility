1f6371e0aa5db914634ffbc0a2babce9ba992ea4
ipointer2 <chris.e.frank@gmail.com>
2018-11-16 12:45:44 -0500

    report printing to the screen now

--- src/components/fertility/Report.js
+++ src/components/fertility/Report.js
@@ -19,8 +19,6 @@ class Report extends Component {
     }
 
     async componentDidMount() {
-        // const data = await API.get('dev-formyfertilityapi', '/formyfertility/186711b9-e4c4-4e4b-b5f3-aa0d33704fac')
-        // console.log('Chris id ending fac: ', data)
 
         const username = await Auth.currentAuthenticatedUser();
         this.setState(username);
@@ -65,66 +63,14 @@ class Report extends Component {
         const results = await API.graphql(graphqlOperation(query));
         this.setState(results.data);
         console.log(`chris the DynamoDB id is: ${JSON.stringify(this.state.createQuestions.id)}`)
-        // API.graphql(graphqlOperation(query))
-        //     .then((queryResult) => {
-        //         this.setState(queryResult.data);
-        //         console.log(this.state.createQuestions.id);
-        //     }).catch((error) => {
-        //         console.log("No Authenticated User");
-        //         console.log(error.message);
-        //     });
-        // Auth.currentAuthenticatedUser()
-        //     .then((user) => {
-        //         this.setState(user);
-        //         console.log(this.state.username);
-        //     }).catch((error) => {
-        //         console.log("No Authenticated User");
-        //         console.log(error.message);
-        //     });
     }
 
     handleSubmit() {
-        let query = `
-            mutation add {
-                createQuestions(input: {
-                    username: "${this.state.username}",
-                    gender: "female",
-                    age: ${this.props.fertilityQuestions.age},
-                    yearChildlessSex: "${this.props.fertilityQuestions.yearChildlessSex}",
-                    amountYearsChildlessSex: ${this.props.fertilityQuestions.amountYearsChildlessSex},
-                    currentIVF: "${this.props.fertilityQuestions.currentIVF}",
-	                hadPregnancy: "${this.props.fertilityQuestions.hadPregnancy}",
-	                hadEctopicPregnancy: "${this.props.fertilityQuestions.hadEctopicPregnancy}",
-	                liveBirth: "${this.props.fertilityQuestions.liveBirth}",
-	                miscarriages: "${this.props.fertilityQuestions.miscarriages}",
-	                partner: "${this.props.fertilityQuestions.partner}",
-                    whichPartnerIssue: "${this.props.fertilityQuestions.whichPartnerIssue}"
-                    amountChildren: ${this.props.fertilityQuestions.amountChildren},
-    	            amountperfecthealth: ${this.props.fertilityQuestions.amountperfecthealth},
-    	            donorsperm: "${this.props.fertilityQuestions.donorsperm}",
-   		            donoreggs: "${this.props.fertilityQuestions.donoreggs}",
-    	            donorembryos: "${this.props.fertilityQuestions.donorembryos}",
-    	            eggs: ${this.props.fertilityQuestions.eggs},
-    	            embryos: ${this.props.fertilityQuestions.embryos},
-    	            embryostransfered: ${this.props.fertilityQuestions.embryostransfered},
-    	            embryosfinalcycle: ${this.props.fertilityQuestions.embryosfinalcycle},
-    	            frozenembryos: "${this.props.fertilityQuestions.frozenembryos}",
-    	            gynecologicalCauses: "${this.props.fertilityQuestions.gynecologicalCauses}",
-    	            gestationalcarrier: "${this.props.fertilityQuestions.gestationalcarrier}",
-    	            hormone: "${this.props.fertilityQuestions.hormone}",
-    	            icsi: "${this.props.fertilityQuestions.icsi}",
-    	            ivfcycles: ${this.props.fertilityQuestions.ivfcycles},
-    	            ivfconceived: "${this.props.fertilityQuestions.ivfconceived}",
-    	            morethanoneivfconceived: "${this.props.fertilityQuestions.morethanoneivfconceived}",
-    	            singletonmulitbirth: "${this.props.fertilityQuestions.singletonmulitbirth}",
-    	            whichGynecologicalCauses: "${this.props.fertilityQuestions.whichGynecologicalCauses}"
-                }) { id }
-            }
-        `
-        API.graphql(graphqlOperation(query))
-            .then((queryResult) => {
-                this.setState(queryResult.data);
-                console.log(this.state.createQuestions.id);
+
+        API.get('dev-formyfertilityapi', `/formyfertility/${this.state.createQuestions.id}`)
+            .then((fertilityResults) => {
+                this.setState({ fertilityResults: JSON.stringify(fertilityResults) });
+                console.log(this.state);
             }).catch((error) => {
                 console.log("No Authenticated User");
                 console.log(error.message);
@@ -135,18 +81,20 @@ class Report extends Component {
         return (
             <Content >
                 <View >
-                    <Text>The Report</Text>
+                    <Text>The Report: </Text>
                 </View>
-                <Form model="fertilityQuestions">
-                    <Control.TextInput model=".age" />
-                    <Control.TextInput model=".yearChildlessSex" />
 
-                    <View>
-                        <Button type="submit" onPress={this.handleSubmit}>
-                            <Text> Get Report!</Text>
-                        </Button>
-                    </View>
-                </Form>
+
+                <View><Text>Your Results: {this.state.fertilityResults}</Text></View>
+
+
+
+                <View>
+                    <Button full rounded primary onPress={this.handleSubmit}>
+                        <Text>Get Report!</Text>
+                    </Button>
+                </View>
+
             </Content>
         );
     }
